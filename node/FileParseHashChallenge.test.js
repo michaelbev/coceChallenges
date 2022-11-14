9718fcdc99ef749e5a906f21d8ab4faf3d16672c
Michael Bevilacqua <mebevilacqua@hotmail.com>
2022-11-14 14:33:51 -0700

    Attempted different variants, still fail

--- node/FileParseHashChallenge.test.js
+++ node/FileParseHashChallenge.test.js
@@ -2,17 +2,28 @@ const FileParseHashChallenge = require('./FileParseHashChallenge')
 
 const test1 = ['FileParseHashChallenge', '0bX78Xa3X9eXe8Xf9Xc4X9bX60X09X00X38XbaX9xXfiXmcX']
 
+// Adding more time does not help
+// jest.setTimeout(10000)
+
+test('FileParseHashChallenge', async () => {
+  expect(await FileParseHashChallenge()).toBe('0bX78Xa3X9eXe8Xf9Xc4X9bX60X09X00X38XbaX9xXfiXmcX')
+})
+
+// These tests await yet still do not work
+
+// test(test1[0], () => {
+//   expect(FileParseHashChallenge()).toBe(test1[1])
+// })
+
+// test(test1[0], async () => {
+//   expect(await FileParseHashChallenge()).toBe(test1[1])
+// })
+
 // test(test1[0], () => {
 //   return FileParseHashChallenge().then(data => {
 //     expect(data).toBe(test1[1])
 //   })
 // })
-
-// jest.setTimeout(3000)
-
-test(test1[0], () => {
-  expect(FileParseHashChallenge(test1[0])).toBe(test1[1])
-})
 
 // test(test1[0], done => {
 //   function callback(error, data) {
